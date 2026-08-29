6e467e02f4bfe05218615d06856467ed6d439e6d
Linh Tuan Nguyen <52875747+silentlie@users.noreply.github.com>
2026-08-29 16:32:00 +1000

    Add user roles and admin middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        abort_unless(
+            $request->user()?->isAdmin(),
+            403
+        );
+
+        return $next($request);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
         return $this->belongsToMany(Post::class, 'votes')
             ->withTimestamps();
     }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
 }
