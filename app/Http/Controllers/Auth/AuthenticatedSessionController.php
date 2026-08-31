ee6183683137a7ffef7a31cf33bfe74dc0477d3b
Linh Tuan Nguyen <52875747+silentlie@users.noreply.github.com>
2026-08-31 14:55:35 +1000

    Simplify navigation and preserve login return path

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -14,8 +14,18 @@ class AuthenticatedSessionController extends Controller
     /**
      * Display the login view.
      */
-    public function create(): View
+    public function create(Request $request): View
     {
+        $redirect = $request->query('redirect');
+
+        if (
+            is_string($redirect) &&
+            str_starts_with($redirect, '/') &&
+            ! str_starts_with($redirect, '//')
+        ) {
+            $request->session()->put('url.intended', $redirect);
+        }
+
         return view('auth.login');
     }
 
@@ -28,7 +38,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('posts.index', absolute: false));
     }
 
     /**
